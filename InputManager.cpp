dd73d3661433fb6538826faca8a6907da71c5963
TheTasak <przemek433@interia.eu>
2017-09-14 10:03:09 +0200

    Update InputManager.cpp

--- InputManager.cpp
+++ InputManager.cpp
@@ -13,4 +13,20 @@ namespace Tas
         }
         return false;
     }
+    bool InputManager::isSpriteMoved(sf::Sprite &object, sf::Mouse::Button button, sf::RenderWindow &window)
+    {
+        sf::Vector2f lastPos = sf::Mouse.getPosition();
+        while(sf::Mouse::isButtonPressed(button))
+        {
+            if(lastPos != sf::Mouse.getPosition();
+            {
+                lastPos = sf::Mouse.getPosition();
+                object.setPosition(lastPos);
+            }
+            sf::IntRect spriteRect(object.getPosition().x, object.getPosition().y, object.getGlobalBounds().width,object.getGlobalBounds().height);
+            if(spriteRect.contains(sf::Mouse::getPosition(window)))
+                return true;
+        }
+        return false;
+    }
 }
